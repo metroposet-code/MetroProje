4afb41120a01ed1c8ed508b8ad8277aff3ee5152
metroposet-code <metroposet@gmail.com>
2026-02-23 23:39:48 +0300

    Add files via upload

---
+++ PlatformGrubu.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Domain;
+
+public sealed class PlatformGrubu
+{
+    public int Id { get; set; }
+    public string GrupAdi { get; set; } = default!;
+    public string? Aciklama { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public List<Platform> Platformlar { get; set; } = new();
+}
